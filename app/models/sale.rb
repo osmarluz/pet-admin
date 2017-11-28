8f2ecdc01295f9a7d6e8b217c8bee16c3404762a
osmarluz <osmar.luz@gmail.com>
2017-11-28 21:28:02 -0200

    Add total sale calculation on sale model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,6 +1,6 @@
 class Sale < ApplicationRecord
   include Fae::BaseModelConcern
-  enum status: { finished: 0, canceled: 1 }
+  enum status: { Finished: 0, Canceled: 1 }
   
   validates :client, presence: true
   belongs_to :discount
@@ -10,6 +10,8 @@ class Sale < ApplicationRecord
   has_many :sale_services
   has_many :services, through: :sale_services
 
+  before_save :set_total
+
   def fae_display_field
     id
   end
@@ -18,4 +20,16 @@ class Sale < ApplicationRecord
     order(:id)
   end
   
+  def set_total
+    total = 0
+    self.products.each { |p| total += p.price }
+    self.services.each { |s| total += s.price }
+ 
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+ 
+    total = (total >= 0) ? total : 0
+    self.total = total
+  end
 end
