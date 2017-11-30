f1172c9fae3ef195efc57fc6006ad940da3d6279
osmarluz <osmar.luz@gmail.com>
2017-11-29 22:47:20 -0200

    Update campaign model to schedule email sending through campaign job

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,11 +3,20 @@ class Campaign < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
   has_many :campaign_clients
   has_many :clients, through: :campaign_clients
+
+  after_save :schedule_emails
 
   def fae_display_field
     title
   end
 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
