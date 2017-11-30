5111795a62dd10f6367724e99b6bb8b2a5a91096
osmarluz <osmar.luz@gmail.com>
2017-11-29 22:00:15 -0200

    Create many-to-many relation between clients and campaigns

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,6 +9,9 @@ class Client < ApplicationRecord
       with: Fae.validation_helpers.email_regex,
       message: 'The email format is invalid and/or this email is already taken'
     }
+  
+  has_many :campaign_clients
+  has_many :campaigns, through: :campaign_clients, dependent: :destroy
 
   def fae_display_field
     name
